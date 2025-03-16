e3de244feb2a272eb25f04df968b8c4a6255b114
Smo7e <romayacovlev@gmail.com>
2025-03-16 17:56:21 +0400

    123

--- src/modules/Scene/components/BackWall/BackWall.tsx
+++ src/modules/Scene/components/BackWall/BackWall.tsx
@@ -3,7 +3,7 @@ import { DoubleSide } from "three";
 
 const BackWall: React.FC = () => {
     return (
-        <mesh position={[0, 5, -5]} rotation={[0, 0, 0]}>
+        <mesh position={[0, 5, -5]} rotation={[0, 0, 0]} receiveShadow>
             <planeGeometry args={[10, 10]} />
             <meshPhongMaterial color={"#BD33A4"} side={DoubleSide} />
         </mesh>
